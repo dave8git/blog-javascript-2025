87d6a1473741038e550dd27f95f7762912cc123c
dave8git <dave8git@gmail.com>
2025-05-04 16:47:35 +0200

    Add clicking on links to articles works

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const titleClickHandler = function(event){
+    event.preventDefault();
+    for (let article of allArticles){
+        article.classList.remove('active');
+    }
+    this.classList.add('active');
+    const activeArticleSelector = this.getAttribute('href');
+    removeArticlePostsActive();
+    document.querySelector(activeArticleSelector).classList.add('active');
+}
+
+const removeArticlePostsActive = function(){
+    const allArticlePosts = document.querySelectorAll('article.post');
+    for (let articlePost of allArticlePosts){
+        articlePost.classList.remove('active');
+    }
+}
+
+const allArticles = document.querySelectorAll('.titles a');
+
+for (let article of allArticles) {
+    article.addEventListener('click', titleClickHandler);
+}
