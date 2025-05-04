5468619647c3be6eb99f188d5f1184c65e10c382
dave8git <dave8git@gmail.com>
2025-05-04 17:35:15 +0200

    Add generating links

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 const titleClickHandler = function(event){
     event.preventDefault();
-    for (let article of allArticles){
-        article.classList.remove('active');
+    for (let link of allLinks){
+        link.classList.remove('active');
     }
     this.classList.add('active');
     const activeArticleSelector = this.getAttribute('href');
@@ -11,15 +11,30 @@ const titleClickHandler = function(event){
     document.querySelector(activeArticleSelector).classList.add('active');
 }
 
+const allArticlePosts = document.querySelectorAll('article.post');
+console.log(allArticlePosts);
+const generateTitleLinks = function(){
+    const titleList = document.querySelector('.list.titles');
+    let htmlLinks = '';
+    for (let article of allArticlePosts) {
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector('.post-title').innerHTML
+        const htmlLink = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+        htmlLinks += htmlLink;
+    }
+    titleList.innerHTML = htmlLinks;
+}
+
+generateTitleLinks();
+
 const removeArticlePostsActive = function(){
-    const allArticlePosts = document.querySelectorAll('article.post');
     for (let articlePost of allArticlePosts){
         articlePost.classList.remove('active');
     }
 }
 
-const allArticles = document.querySelectorAll('.titles a');
+const allLinks = document.querySelectorAll('.titles a');
 
-for (let article of allArticles) {
-    article.addEventListener('click', titleClickHandler);
+for (let link of allLinks){
+    link.addEventListener('click', titleClickHandler);
 }
