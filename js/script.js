fb4c8b187ac4d3ad4ef5f264e7e256e253abfc87
dave8git <dave8git@gmail.com>
2025-05-05 21:31:25 +0200

    Fix deleted unnecessary conole.log

--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    event.preventDefault();
-    for (let link of allLinks){
-        link.classList.remove('active');
-    }
-    this.classList.add('active');
-    const activeArticleSelector = this.getAttribute('href');
-    removeArticlePostsActive();
-    document.querySelector(activeArticleSelector).classList.add('active');
-}
+  event.preventDefault();
+  for (let link of allLinks){
+    link.classList.remove('active');
+  }
+  this.classList.add('active');
+  const activeArticleSelector = this.getAttribute('href');
+  removeArticlePostsActive();
+  document.querySelector(activeArticleSelector).classList.add('active');
+};
 
 const allArticlePosts = document.querySelectorAll('article.post');
-console.log(allArticlePosts);
 const generateTitleLinks = function(){
-    const titleList = document.querySelector('.list.titles');
-    let htmlLinks = '';
-    for (let article of allArticlePosts) {
-        const articleId = article.getAttribute('id');
-        const articleTitle = article.querySelector('.post-title').innerHTML
-        const htmlLink = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
-        htmlLinks += htmlLink;
-    }
-    titleList.innerHTML = htmlLinks;
-}
+  const titleList = document.querySelector('.list.titles');
+  let htmlLinks = '';
+  for (let article of allArticlePosts) {
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector('.post-title').innerHTML;
+    const htmlLink = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+    htmlLinks += htmlLink;
+  }
+  titleList.innerHTML = htmlLinks;
+};
 
 generateTitleLinks();
 
 const removeArticlePostsActive = function(){
-    for (let articlePost of allArticlePosts){
-        articlePost.classList.remove('active');
-    }
-}
+  for (let articlePost of allArticlePosts){
+    articlePost.classList.remove('active');
+  }
+};
 
 const allLinks = document.querySelectorAll('.titles a');
 
 for (let link of allLinks){
-    link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
